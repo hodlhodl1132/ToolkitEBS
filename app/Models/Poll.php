c447d5f4801535d8cd5593acf1c6b35555ce26f9
donavyn elliott <donavyn.elliott@gmail.com>
2022-06-03 19:48:22 -0700

    Added pruning to models

--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class Poll extends Model
 {
-    use HasFactory;
+    use HasFactory, Prunable;
 
     protected $fillable = [
         'provider_id',
@@ -20,4 +23,15 @@ class Poll extends Model
     protected $casts = [
         'options' => 'array'
     ];
+
+    /**
+     * Determines the prunable query
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function prunable()
+    {
+        $currentTime = intval(Carbon::now()->timestamp) + 20;
+        return $this->where('end_time', '<=', $currentTime);
+    }
 }
