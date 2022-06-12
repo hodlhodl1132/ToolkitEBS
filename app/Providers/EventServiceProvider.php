e8662134dfc0a8e416f5610a57ef7133ed7fd260
donavyn elliott <donavyn.elliott@gmail.com>
2022-06-11 23:13:54 -0700

    Added twitch socialite provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,7 +16,11 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ]
+        ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // ... other providers
+            \SocialiteProviders\Twitch\TwitchExtendSocialite::class . '@handle',
+        ],
     ];
 
     /**
