8d96009b09fc1ccddbc8326807449d7baad1f49f
donavyn elliott <donavyn.elliott@gmail.com>
2022-06-13 17:47:51 -0700

    List all users accounts in paginated table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -11,6 +12,10 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('users.index');
+        $users = User::paginate(10);
+
+        return view('users.index', [
+            'users' => $users
+        ]);
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,4 +4,23 @@
             {{ __('User Accounts') }}
         </h2>
     </x-slot>
+
+    <table class="ui celled table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Provider ID</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td data-label="Name">{{ $user->name }}</td>
+                    <td data-label="Provider Id">{{ $user->provider_id }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    {{ $users->links() }}
 </x-app-layout>
