166dc7dae140ebb7f6e7790467e8b068992e75b6
donavyn elliott <donavyn.elliott@gmail.com>
2022-06-19 13:44:44 -0700

    Added responsive view for user editor

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,91 +6,87 @@
     </x-slot>
 
     @if ($errors->any())
-        <div class="ui warning message">
-            <i class="close icon"></i>
-              <div class="header">
-                There were issues validating your submission
-            </div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+        @include('components.display-errors')
     @endif
 
-    <div class="ui cards">
-        <div class="card">
-            <div class="content">
-                <div class="header">{{ $user->name }}</div>
-                <div class="meta">Streamer</div>
+    <div class="ui grid stackable permissions-container">
+        
+        <div class="twelve wide column">
+            <div class="ui cards">
+                <div class="card">
+                    <div class="content">
+                        <div class="header">{{ $user->name }}</div>
+                        <div class="meta">Streamer</div>
+                    </div>
+                        <a href="{{ 'https://www.twitch.tv/' . $user->name }}" target="_blank" >
+                        <div class="ui purple bottom attached button">
+                        <i class="white twitch icon"></i>
+                            Twitch
+                        </div>
+                        <a>
+                </div>
             </div>
-                <a href="{{ 'https://www.twitch.tv/' . $user->name }}" target="_blank" >
-                <div class="ui purple bottom attached button">
-                <i class="white twitch icon"></i>
-                    Twitch
-                </div>
-                <a>
         </div>
-    </div>
-    <div class="container permissions-container">
-        <h3 class="ui header">Permissions</h3>
-        <table class="ui very basic collapsing table">
-            <thead>
-                <tr>
-                    <th>Permission Name</th>
-                    <th>Remove</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr><td><h5 class="ui header">Role Permissions</h5></td></tr>
-                @foreach ($user->getPermissionsViaRoles() as $perm)
-                    <tr>
-                        <td aria-label="permission-name">{{ $perm->name }}</td>
-                        <td aria-label="remove"></td>
-                    </tr>
-                @endforeach
-                <tr><td><h5 class="ui header">Direct Permissions</h5></td></tr>
-                @foreach ($user->getDirectPermissions() as $perm)
-                    <tr>
-                        <td aria-label="permission-name">{{ $perm->name }}</td>
-                        <td aria-label="remove">
-                            @can('admin.users.edit')
-                                <form method="POST" action="{{ route('permission.delete') }}">
-                                    @csrf
-                                    <input type="hidden" value="{{ $perm->id }}" name="id">
-                                    <input type="hidden" value="{{ $user->id }}" name="user_id">
-                                    <a :href="{{ route('permission.delete') }}">
-                                    <i onclick="event.preventDefault();
-                                        this.closest('form').submit();"
-                                        class="trash alternate icon"></i>
-                                    </a>
-                                </form>
-                            @endcan
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
 
-        @can('admin.users.edit')
-        <div class="ui form">
-            <form method="POST" action="{{ route('permission.store') }}">
-                @csrf
-                <input type="hidden" value="{{ $user->id }}" name="user_id">
-                <div class="field">
-                    <label>Permission</label>
-                    <select class="ui search dropdown" name="id">
-                        <option value="">Select Permission</option>
-                        @foreach ($permissions as $perm)
-                            <option value="{{ $perm->id }}">{{ $perm->name }}</option>
-                        @endforeach
-                    </select>
+        <div class="four wide column">
+            <h3 class="ui header">Permissions</h3>
+            <table class="ui very basic collapsing table">
+                <thead>
+                    <tr>
+                        <th>Permission Name</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td><h5 class="ui header">Role Permissions</h5></td></tr>
+                    @foreach ($user->getPermissionsViaRoles() as $perm)
+                        <tr>
+                            <td aria-label="permission-name">{{ $perm->name }}</td>
+                            <td aria-label="remove"></td>
+                        </tr>
+                    @endforeach
+                    <tr><td><h5 class="ui header">Direct Permissions</h5></td></tr>
+                    @foreach ($user->getDirectPermissions() as $perm)
+                        <tr>
+                            <td aria-label="permission-name">{{ $perm->name }}</td>
+                            <td aria-label="remove">
+                                @can('admin.users.edit')
+                                    <form method="POST" action="{{ route('permission.delete') }}">
+                                        @csrf
+                                        <input type="hidden" value="{{ $perm->id }}" name="id">
+                                        <input type="hidden" value="{{ $user->id }}" name="user_id">
+                                        <a :href="{{ route('permission.delete') }}">
+                                        <i onclick="event.preventDefault();
+                                            this.closest('form').submit();"
+                                            class="trash alternate icon"></i>
+                                        </a>
+                                    </form>
+                                @endcan
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            @can('admin.users.edit')
+                <div class="ui form">
+                    <form method="POST" action="{{ route('permission.store') }}">
+                        @csrf
+                        <input type="hidden" value="{{ $user->id }}" name="user_id">
+                        <div class="field">
+                            <label>Permission</label>
+                            <select class="ui search dropdown" name="id">
+                                <option value="">Select Permission</option>
+                                @foreach ($permissions as $perm)
+                                    <option value="{{ $perm->id }}">{{ $perm->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <button class="ui primary button" tabindex="0">{{ __('Add Permission') }}</button>
+                    </form>
                 </div>
-                <button class="ui primary button" tabindex="0">{{ __('Add Permission') }}</button>
-            </form>
+            @endcan
         </div>
-        @endcan
 
     </div>
 </x-app-layout>
